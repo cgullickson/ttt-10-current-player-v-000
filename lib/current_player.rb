b0ac2fd9c893565ac02327ea1b94cf34439d8edd
Chris Gullickson <cgullickson5@gmail.com>
2017-11-10 17:09:03 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+board = [" "," "," "," "," "," "," "," "," "]
+
+def turn_count(board)
+  board.each do |space|
+    counter = 0
+    if 
+      space == "X" || space == "O"
+      counter += 1
+    end
+end
+  
